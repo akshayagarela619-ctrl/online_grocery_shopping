e05493cece657fb6c31f382f175598b6225d5c2c
Akshaya Garela <Akshaya Garela-akshayagarela619@gmail.com>
2025-11-17 22:32:05 +0530

    Fix orders template: use SimpleNamespace so o.items is a list

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from types import SimpleNamespace
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 import os
@@ -258,7 +259,8 @@ def my_orders():
                 (o["order_id"],)
             ).fetchall()
 
-            orders_list.append({"order": o, "items": items})
+            orders_list.append(SimpleNamespace(order=o, items=items))
+
 
         conn.close()
         return render_template("orders.html", orders=orders_list)
